2a589b513fde49a6853b3a5e632d4f955fc7f693
Jiale-Yue <1012498719@qq.com>
2021-03-16 10:53:35 +0800

    update

---
+++ session2/bootstrap  Exercise/singal-page.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery-2.1.4.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+ <div class="row">
+     <img class="img" src="images/banner.jpeg" alt="">
+ </div>
+ <div class="row">
+ <div class="col-lg-4">
+<p>Menu item1</p>
+<p>Menu item2</p>
+<p>Menu item3</p>
+ </div>
+ <div class="col-lg-8">
+<p>content</p>
+ </div>
+ </div>
+</div><!-- content container -->
+
+</body>
+</html>
